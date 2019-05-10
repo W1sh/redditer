03a8d5abce587b2d835c1b493ce083a69689fdae
W1sh <brunoferreira.ebm@gmail.com>
2019-05-10 20:05:31 +0100

    started implementing redditer, created 2 enums for category and time

--- redditer.php
+++ redditer.php
@@ -3,16 +3,40 @@ require_once "./vendor/autoload.php";
 
 use am\internet\HttpHelper;
 
-class Redditer {
-    private $mHttpHelper;
+abstract class Category{
+    const cHot = "hot";
+    const cNew = "new";
+    const cControversial = "controversial";
+    const cTop = "top";
+    const cRising = "rising";
+}
 
-    const REDDIT_SEARCH_BASE = "https://www.reddit.com/search?q=";
+abstract class Time{
+    const tHour = "hour";
+    const tday = "day";
+    const tWeek = "week";
+    const tMonth = "month";
+    const tYear = "year";
+    const tAll = "all";
+}
+
+class Redditer {
+    public $mHttpHelper;
+
+    const REDDIT_SEARCH_BASE = "https://www.reddit.com/r/%s/%s.json?t=%s";
 
     public function __construct(){
         $this->mHttpHelper = new HttpHelper(HttpHelper::USER_AGENT_STRING_MOZ47);
     }//__construct
 
-
+    public function build_query($pSubreddit, $pCategory, $pTime) : string{
+        return sprintf(self::REDDIT_SEARCH_BASE, $pSubreddit, $pCategory, $pTime);
+    }
 }// redditer
 
 $r = new Redditer();
+$json = $r->mHttpHelper->http(sprintf($r::REDDIT_SEARCH_BASE, "apexlegends", "hot", "day"))[HttpHelper::KEY_BIN];
+$url = sprintf($r::REDDIT_SEARCH_BASE, "apexlegends", Category::cHot, Time::tday);
+echo $url.PHP_EOL;
+$oJson = json_decode($json);
+var_dump($oJson);
---
+++ wordpresser.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Artur Marques
+ */
+require_once "./vendor/autoload.php";
+
+use wordpresser\am_wordpress_tools;
+
+date_default_timezone_set("Europe/Lisbon");
+
+$BLOG = array("blogname"=>"SUPA ARTIGO 33", "user"=>"admin", "pass"=>"1234", "blogxmlrpc"=>"http://localhost:9000/xmlrpc.php");
+
+define ("BLOG_USER", $BLOG['user']);
+
+/*
+ * user ou password errada dará
+ * @wordpress_postToBlog : Something went wrong - 403 : Incorrect username or password.
+ *
+ * um post com data futura fica com post_status "future" e só aparecerá quando o futuro chegar...
+ */
+define ("BLOG_PASS", $BLOG['pass']);
+define ("BLOG_XMLRPC", $BLOG['blogxmlrpc']);
+
+$ret = wordpress_postToBlog (
+    $title = "um post via AM's Wordpress Tools",
+    $body = "Funciona?",
+    $cats = "test, testing",
+    $keywordsString = "wordpresser, bot",
+    $featuredImageId=null,
+    $dateCreatedInt = mktime(
+        $hour=16,
+        $minutes=36,
+        $seconds=15,
+        $month=4,
+        $day=27,
+        $year=2019
+    ),
+    //$dateCreatedInt = "2017-05-08", //para a data de agora desde que a timezone esteja correta no Wordpress
+    $allowComments=true,
+    $allowPings=true,
+    $user=BLOG_USER,
+    $pass=BLOG_PASS,
+    $blogXmlRpcDotPhpFullUrl=BLOG_XMLRPC
+);
+/*
+ * em caso de sucesso
+ * @wordpress_postToBlog : Posted OK!
+ */
+
+var_dump ($ret);
