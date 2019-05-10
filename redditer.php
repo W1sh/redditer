cff25de685b218dcd81ae4a56138bd0e06e5c53b
W1sh <brunoferreira.ebm@gmail.com>
2019-05-10 18:05:11 +0100

    built redditer base

--- redditer.php
+++ redditer.php
@@ -1,15 +1,18 @@
 <?php
 require_once "./vendor/autoload.php";
 
+use am\internet\HttpHelper;
+
 class Redditer {
     private $mHttpHelper;
 
+    const REDDIT_SEARCH_BASE = "https://www.reddit.com/search?q=";
+
     public function __construct(){
-        $this->mHttpHelper = new \libs\helpers\am\internet\HttpHelper();
-        $this->mHttpHelper = new HttpHelper(HttpHelper::USER_AGENT_STRING_MOZ47//,
-            //false
-        );
+        $this->mHttpHelper = new HttpHelper(HttpHelper::USER_AGENT_STRING_MOZ47);
     }//__construct
+
+
 }// redditer
 
 $r = new Redditer();
