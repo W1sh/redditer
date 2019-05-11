a8509793adc7891f92f0110468f5f0fa23137b90
W1sh <brunoferreira.ebm@gmail.com>
2019-05-11 10:45:43 +0100

    built base for a reddit result

--- redditer.php
+++ redditer.php
@@ -13,7 +13,7 @@ abstract class Category{
 
 abstract class Time{
     const tHour = "hour";
-    const tday = "day";
+    const tDay = "day";
     const tWeek = "week";
     const tMonth = "month";
     const tYear = "year";
@@ -23,20 +23,32 @@ abstract class Time{
 class Redditer {
     public $mHttpHelper;
 
-    const REDDIT_SEARCH_BASE = "https://www.reddit.com/r/%s/%s.json?t=%s";
+    const REDDIT_SEARCH_BASE = "https://www.reddit.com/r/%s/%s.json?t=%s&limit=%d";
 
     public function __construct(){
         $this->mHttpHelper = new HttpHelper(HttpHelper::USER_AGENT_STRING_MOZ47);
     }//__construct
 
-    public function build_query($pSubreddit, $pCategory, $pTime) : string{
-        return sprintf(self::REDDIT_SEARCH_BASE, $pSubreddit, $pCategory, $pTime);
+    public function build_query($pSubreddit, $pCategory=Category::cHot, $pTime=Time::tDay, $pLimit=10) : string{
+        return sprintf(self::REDDIT_SEARCH_BASE, $pSubreddit, $pCategory, $pTime, $pLimit);
+    }
+
+    public function fetch_from_json($pJson) {
+        $posts = $pJson->data->children;
+        foreach ($posts as $keys){
+            echo $keys->data->title;
+            //echo $keys->data->selftext;
+            echo $keys->data->ups;
+            echo $keys->data->total_awards_received;
+            echo $keys->data->author;
+            echo $keys->data->num_comments;
+            echo $keys->data->permalink;
+            echo $keys->data->url;
+        }
     }
 }// redditer
 
 $r = new Redditer();
-$json = $r->mHttpHelper->http(sprintf($r::REDDIT_SEARCH_BASE, "apexlegends", "hot", "day"))[HttpHelper::KEY_BIN];
-$url = sprintf($r::REDDIT_SEARCH_BASE, "apexlegends", Category::cHot, Time::tday);
-echo $url.PHP_EOL;
+$json = $r->mHttpHelper->http($r->build_query("apexlegends", Category::cTop, Time::tAll))[HttpHelper::KEY_BIN];
 $oJson = json_decode($json);
-var_dump($oJson);
+$r->fetch_from_json($oJson);
