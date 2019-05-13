832a67e3e31969edf50fdebedb792fc5546a8537
W1sh <brunoferreira.ebm@gmail.com>
2019-05-13 12:35:40 +0100

    finished collecting comments

--- redditer.php
+++ redditer.php
@@ -26,7 +26,7 @@ class Redditer {
             /*echo $keys->data->title;
             //echo $keys->data->selftext;
             //echo $keys->data->selftext_html;
-            echo $keys->data->ups;
+            echo $keys->data->ups; // maybe use ->score instead
             echo $keys->data->total_awards_received;
             echo $keys->data->author;
             echo $keys->data->num_comments;
@@ -35,7 +35,7 @@ class Redditer {
             echo $keys->data->over_18;
             echo $keys->data->spoiler;
             echo $keys->data->created_utc;
-            // gmdate ("r", $created_utc);*/
+            // gmdate ("d-m-Y h:m", $created_utc);*/
         }
         echo $permalink.PHP_EOL;
         $json = $this->get_json($permalink);
@@ -45,6 +45,7 @@ class Redditer {
         $mostUpvoted = $this->find_most_upvoted_comment($this->mCommentsList);
         $mostControversial = $this->find_most_controversial_comment($this->mCommentsList);
         $mostAwarded = $this->find_most_awarded_comment($this->mCommentsList);
+
     }// fetch_from_json
 
     private function get_json($pUrl) {
@@ -59,7 +60,7 @@ class Redditer {
             $jScore = $comment->data->score;
             $jAuthor = $comment->data->author;
             $jContent = $comment->data->body;
-            $jCreated = gmdate("r", $comment->data->created_utc);
+            $jCreated = gmdate("d-m-Y h:m", $comment->data->created_utc);
             $jReplies = $comment->data->replies != "" ? $comment->data->replies->data->children : null;
             if(is_array($jReplies) && count($jReplies)>0){
                 $jNumReplies = count($comment->data->replies->data->children);
@@ -76,16 +77,25 @@ class Redditer {
         }
     }
 
-    private function find_most_awarded_comment($pArrayComments) : RedditComment{
-        return RedditComment;
+    private function find_most_awarded_comment($pArrayComments){
+        usort($pArrayComments, function ($a, $b){
+            return ($a->awards > $b->awards) ? -1 : 1;
+        });
+        return $pArrayComments[0];
     }//find_most_awarded_comment
 
-    private function find_most_controversial_comment($pArrayComments) : RedditComment{
-        return RedditComment;
+    private function find_most_controversial_comment($pArrayComments){
+        usort($pArrayComments, function ($a, $b){
+            return ($a->replies > $b->replies) ? -1 : 1;
+        });
+        return $pArrayComments[0];
     }//find_most_controversial_comment
 
-    private function find_most_upvoted_comment($pArrayComments) : RedditComment{
-        return RedditComment;
+    private function find_most_upvoted_comment($pArrayComments){
+        usort($pArrayComments, function ($a, $b){
+            return ($a->score > $b->score) ? -1 : 1;
+        });
+        return $pArrayComments[0];
     }//find_most_upvoted_comment
 
     private function build_title($pTitle, $pOver18=false, $pSpoiler=false) : string{
